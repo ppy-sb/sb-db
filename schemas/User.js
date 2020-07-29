c5de24de4b271dae0c54d0e862e8a3778e388310
Nao_ke <nao_ke@qq.com>
2020-07-29 23:40:57 +0800

    username -> name

--- schemas/User.js
+++ schemas/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 module.exports = new mongoose.Schema({
     id: { type: Number, index: true },
-    username: { type: String, index: true },
+    name: { type: String, index: true },
     password: String,
 })
