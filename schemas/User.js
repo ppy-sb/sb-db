bd8cbd62d6d093e9e4fce121ff3fed6820c72717
Nao_ke <nao_ke@qq.com>
2020-08-06 01:30:28 +0800

    add verify method

--- schemas/User.js
+++ schemas/User.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
-module.exports = new mongoose.Schema({
+const schema = new mongoose.Schema({
     id: { type: Number, unique: true },
     name: { type: String, unique: true },
     nick: { type: String, unique: true },
 })
+
+schema.methods.verify = async (name, password) => {
+    const user = await mongoose.model('User').findOne({name}).exec()
+    if(!user) return
+    if(!await mongoose.model('Account').exists({ user:{ _id: user._id }, password })) return
+    return user
+}
+
+module.exports = schema
