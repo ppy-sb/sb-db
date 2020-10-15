a77545acca9b1781c7daca13564f95d75ed82780
arily <lbsaaaaa@gmail.com>
2020-10-15 15:29:21 +0900

    Update Log.js

--- schemas/Log.js
+++ schemas/Log.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose')
 module.exports = new mongoose.Schema({
     event: { type: String, required: true, index: true },
     id: { type: String, required: true, index: true },
-    parameters: {type: Array, default: () => []}
+    parameters: {type: Array, default: () => []},
+    time: {type: Date, default: Date.now}
 })
